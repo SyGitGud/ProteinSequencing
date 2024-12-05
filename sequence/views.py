52536fc1ec0d37fbae14f35085df11f15723bed1
Sy <96253671+SyGitGud@users.noreply.github.com>
2024-12-05 14:10:08 -0500

    Update views.py
    KNN request

--- sequence/views.py
+++ sequence/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from .knn_model import predict_sequence, gap_fill_sequence, knn_model
 from . import login
 
-def home(request):
-    return render(request, 'home.html') #send back our home document
 def login_views(request):
     username = ''
     password = ''
@@ -16,3 +15,15 @@ def login_views(request):
     else:
         messages.error(request, 'Username or Password is incorrect!')
     return render(request, 'loginpage.html')
+def home(request):
+    prediction = None  # Default value for prediction
+
+    if request.method == 'POST':
+        user_seq = request.POST.get('user_seq')  # Get the sequence input from the form
+        
+        if user_sequence:  # Only process if a sequence is provided
+            # Use the gap_fill_sequence function to predict the filled sequence
+            prediction = gap_fill_sequence(user_seq, knn_model)  # Pass the sequence and model
+
+    # Render the home page with the prediction
+    return render(request, 'home.html', {'prediction': prediction})
