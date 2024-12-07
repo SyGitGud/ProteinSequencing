6800de76f5768ccaa1da12f90b00004c7a4f2a78
Sy <96253671+SyGitGud@users.noreply.github.com>
2024-12-06 19:47:55 -0500

    Update urls.py

--- sequence/urls.py
+++ sequence/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'), 
     path('login/', views.loginpage, name='loginpage'),  
+    path('infopage/', views.infopage, name = 'infopage')
 ]
