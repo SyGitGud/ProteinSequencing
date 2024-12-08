5cdde6835cf3d38596b59d824500bd20ad3106a5
Sy <96253671+SyGitGud@users.noreply.github.com>
2024-12-08 12:14:45 -0500

    Final Debugging

--- gen_env.ps1
+++ gen_env.ps1
@@ -1,3 +1,12 @@
 Write-Host "secret key being made!..."
-$SECRET_KEY = python -c "from django.core.management.utils import get_random_secret_key; get_random_secret_key()"
-Set-Content -Path .env -Value "SECRET_KEY=$SECRET_KEY"
+try {
+    $SECRET_KEY = python -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())"
+    Set-Content -Path .env -Value "SECRET_KEY=$SECRET_KEY"
+    Write-Host "SECRET_KEY created."
+}
+catch {
+    Write-Host "UH OH!"
+}
+
+
+
