1974685cc732dc57f4c018f1c2dba6bed4a9c601
Sy <96253671+SyGitGud@users.noreply.github.com>
2024-12-08 04:28:50 -0500

    Added logout and history
    URLs to the views for history and logout

--- sequence/urls.py
+++ sequence/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'), 
     path('login/', views.loginpage, name='loginpage'),  
-    path('infopage/', views.infopage, name = 'infopage')
+    path('infopage/', views.infopage, name = 'infopage'),
+    path('logout/', views.log_out, name='log_out'),
+    path('history/', views.history, name='history')
 ]
