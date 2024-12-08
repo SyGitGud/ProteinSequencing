4fba13aec765a5b2621ab9dea57d17bc5dcf2e84
Sy <96253671+SyGitGud@users.noreply.github.com>
2024-12-08 15:33:09 -0500

    delete function url

--- sequence/urls.py
+++ sequence/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', views.loginpage, name='loginpage'),  
     path('infopage/', views.infopage, name = 'infopage'),
     path('logout/', views.log_out, name='log_out'),
-    path('history/', views.history, name='history')
+    path('history/', views.history, name='history'),
+    path('delete/', views.delete_account, name='delete_account')
 ]
