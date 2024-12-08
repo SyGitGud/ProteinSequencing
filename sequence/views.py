b7abff9253dad6101bff2e5ee755c82c5843c8e2
Sy <96253671+SyGitGud@users.noreply.github.com>
2024-12-08 04:26:29 -0500

    Working views
    Now accepts logout, download, cleared history, and handled the model predictions.

--- sequence/views.py
+++ sequence/views.py
@@ -2,11 +2,17 @@ from django.apps import apps
 from .apps import SequenceConfig  # Import from apps
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.http import HttpResponse
+import csv
 from django.contrib.auth.decorators import login_required
-from .knn_model_for_protein_scaffold.knn_model import predict_sequence, loaded_model
+from .knn_model_for_protein_scaffold.knn_model import predict_seq, calc_accuracy
 from .models import History
+
+def log_out(request):
+    logout(request)
+    return redirect(loginpage)
 
 def loginpage(request):
     if request.method == 'POST':
@@ -14,10 +20,9 @@ def loginpage(request):
         password = request.POST.get('password')
 
         if 'login_button' in request.POST:
-            user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password) #authenticat them
             if user: #check if this user is successfully made and added to Django User model
-                login(request, user) #logged in user session
-                messages.success(request, f"Welcome back, {user.username}!")
+                login(request, user) #logged in user session use django built in
                 return redirect('home')  # Redirect to the home view
             else:
                 messages.error(request, "Username or Password is incorrect!")
@@ -37,10 +42,44 @@ def loginpage(request):
     return render(request, 'loginpage.html')
 
 
+def history(request): #downloading history and clearing it
+    user = request.user
+
+    if request.method == 'POST':
+        hist = request.POST.get('hist') #getting the values by name
+        if 'Download' in hist: 
+            history_file = HttpResponse(content_type='text/csv')
+            history_file['Content-Disposition'] = 'attachment; filename="scaffold_history.csv"' #name and create a csv file
+            csv_edit = csv.writer(history_file)
+            csv_edit.writerow(['timestamp', 'gap scaffold', 'filled sequence'])
+            records = History.objects.filter(user=user).all()
+
+            for record in records:
+                csv_edit.writerow([record.created_at, record.input_sequence, record.prediction_result]) #populate the csv file
+            
+            return history_file
+
+        if 'Clear' in hist:
+                records = History.objects.filter(user=user) #delete all of that user's entries
+                records.delete()
+                return redirect('home')
+
+
+
+
+
+
+
 @login_required
 def home(request):
     prediction = None  # Default value for prediction
+    accurate = None
+    train_accuracy = None
     history = None
+    user_seq = None
+
+
+
 
     if request.method == 'POST':
         print("Form submitted")
@@ -48,18 +87,25 @@ def home(request):
         
         if user_seq:
             print(user_seq)
-            config_seq = apps.get_app_config('sequence')
-            knn_classifier, CHAR_TO_INT, INT_TO_CHAR =  config_seq.get_knn_model()
-            prediction = predict_sequence(user_seq, knn_classifier, CHAR_TO_INT, INT_TO_CHAR)
+            prediction = predict_seq(user_seq)
             if prediction:
                 History.objects.create(user = request.user, input_sequence=user_seq, prediction_result=prediction)
-                print(f"Predicted Sequence: {prediction}")  
-    
+                print(f"Predicted Sequence: {prediction}")
+                train_accuracy, accurate = calc_accuracy()
+                train_accuracy = f"{train_accuracy *100:.2f}%"
+                accurate = f"{accurate*100:.2f}%" 
+   
+
+
+
+
+
+    return render(request, 'home.html', {'prediction': prediction, 'train_accuracy': train_accuracy, 'accurate': accurate, 'scaffold': user_seq, 'user': request.user.username})
     
     # Home page with prediction (if applicable)
-    return render(request, 'home.html', {'prediction': prediction})
 
-def infopage(request):
+
+def infopage(request): #information page
 
     if 'home_button' in request.POST:
         return redirect('home')
