3a67657c6c3fd10c6eb927a6e81efb22946be772
Sy <96253671+SyGitGud@users.noreply.github.com>
2024-12-08 10:02:25 -0500

    Update settings.py
    configure the secret key

--- protein_proj/settings.py
+++ protein_proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^^mbxcm#!3f_-+5brvn%3x1b)by)5xp^hidbgmasd&9dfc%*%q'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
